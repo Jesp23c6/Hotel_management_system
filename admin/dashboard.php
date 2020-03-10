5d912638c710c959824549877905d8907c87fce5
jesp23c6 <jesp23c6@tietgen.dk>
2020-03-10 10:53:46 +0100

    refactor: moved dashboard.css to the corresponding folder

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -15,7 +15,7 @@ if($admin=="")
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link href="dashboard.css" rel="stylesheet">
+  <link href="../css/dashboard.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <style>
