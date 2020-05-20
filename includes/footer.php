d37f11f6bb8e34c8471339ceb970a87b6461bdb1
Jesp23c6 <jesp23c6@tietgen.dk>
2020-05-20 12:17:22 +0200

    [Hotfix] minor code adjust

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,12 @@
 <?php 
 include('connection.php');
+
 extract($_REQUEST);
-if(isset($send))
-{
-mysqli_query($con,"insert into feedback values('','$n','$e','$mob','$msg')");	
-$msg= "<h4 style='color:green;'>feedback sent successfully</h4>";
+
+if(isset($send)){
+  mysqli_query($con,"insert into feedback values('','$n','$e','$mob','$msg')");	
+
+  $msg= "<h4 style='color:green;'>feedback sent successfully</h4>";
 }
 ?>
 <!-- Footer1 Start Here-->
@@ -15,9 +17,11 @@ $msg= "<h4 style='color:green;'>feedback sent successfully</h4>";
 		<img src="logo/logo2.png"width="200px"height="50px"/><br><br>
     <p class="text-justify">A hotel is an establishment that provides paid lodging on a short-term basis. Facilities provided may range from a modest-quality mattress in a small room to large suites with bigger, higher-quality beds, a dresser, a refrigerator and other kitchen facilities, upholstered chairs, a flat screen television, and en-suite bathrooms. Small, lower-priced hotels may offer only the most basic guest services and facilities. Larger, higher-priced hotels may provide additional guest facilities such as a swimming pool, business center</p><br>
       <center><a href="about.php" class="btn btn-danger"><b>Read More..</b></a></center><br><br><br>
- <?php
+
+<?php
   include('social_icon.php')
 ?>
+
 	</div>&nbsp;&nbsp;
 	<div class="col-sm-4 text-justify">
 	       <h3 style="color:#cdd51f;">Contact Us</h3>
